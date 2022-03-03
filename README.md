811e29356f2f6ec9d319e0944f568b52483ce1e2
Thiago de Castro <tghcastro@gmail.com>
2022-03-03 20:04:40 +0000

    added some tests focusing in basic cypress commands and assertions

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ Small POC with some learning stuff about Cypress
 
 PS: Page Object was intentionally left aside. The idea was to focus exclusively on Cypress
 
+## Application Under Test
+
+1. Clone this repo git@github.com:cypress-io/cypress-example-todomvc.git
+2. Follow the instructions to install dependencies and run and application
+
 ## Useful commands
 
 ```sheel
-
 # Troubleshooting
 npm cache --force clean
 rm -rf /Users/thiago.castro/Library/Caches/Cypress/
---
+++ cypress/integration/todos_spec.js
@@ -0,0 +1,44 @@
+describe('Add new items', function() {
+    let newItem = "learning cypress"
+
+    beforeEach(function() {
+        // TODO: Something todo before each test
+        cy.visit('/')
+    })
+
+    it('Add a new item', function(){
+        cy.get('.new-todo').type('learn cypress')
+    })
+
+    it('Add two items saving them', function(){
+        cy.get('.new-todo').type(`${newItem}{enter}forget Webdriver{enter}`)
+    })
+
+    context('No Todos', function(){
+        it('Item field footer should not be available', function (){
+            cy.get('.todo-list li').should('not.exist')
+        })
+        it('No item should exist', function (){
+            cy.get('.main').should('not.exist')
+        })
+    })
+
+    context('After item added', function() {
+        beforeEach(function() {
+            cy.get('.new-todo').type(`${newItem}{enter}`)
+        })
+
+        it('should display item in the list', function() {
+            cy.contains(newItem)
+            cy.get('.todo-list li').should('have.length', 1)
+        })
+
+        it('should display all items after adding more', function() {
+            cy.get('.new-todo').type(`One More{enter}`)
+            cy.get('.new-todo').type(`Another One{enter}`)
+            cy.get('.todo-list li').should('have.length', 3)
+        })
+    })
+
+
+})
